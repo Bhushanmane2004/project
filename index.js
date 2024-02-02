bf2287b222cb4d36ed5a77de0a4b75dda661dd1f
Bhushan@0110 <bhushanvijaymane@gmail.com>
2024-02-02 19:25:32 +0530

    Third

--- index.js
+++ index.js
@@ -1,24 +1,74 @@
-import express  from "express";
-import bodypraser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql2";
 
 const app = express();
 const port = 3000;
 
-app.use(bodypraser.urlencoded({extended: true}));
+// Set up body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
- res.render("login.ejs");
+// Create MySQL connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Bhushan@01",
+    database: "Login",
+    insecureAuth: true
 });
 
-app.post("/login",(req,res)=>{
-    const username= req.body.username;
-    const password=req.body.password;
-    console.log(username);
-    console.log(password);
-    res.render("login.ejs");
+// Check MySQL connection
+db.connect(err => {
+    if (err) {
+        console.error("Error connecting to database:", err);
+    } else {
+        console.log("Connected to database");
+    }
 });
 
-app.listen(port,()=>{
+app.get("/", (req, res) => {
+    res.render("login.ejs"); // Assuming you have login.ejs in your views directory
+});
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    
+    // Check if the user exists
+    db.query("SELECT * FROM User WHERE Username = ?", [username], (err, result) => {
+        if (err) {
+            console.error("Error executing query:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+
+        if (result.length > 0) {
+            console.log("User exists");
+            const storedPassword = result[0].Password;
+            // Compare passwords or perform any necessary authentication here
+            if (password === storedPassword) {
+                console.log("Password matched");
+                res.send("Login successful");
+            } else {
+                console.log("Password does not match");
+                res.send("Incorrect password");
+            }
+        } else {
+            console.log("User does not exist");
+            // Create new user
+            db.query("INSERT INTO User (Username, Password) VALUES (?, ?)", [username, password], (err, result) => {
+                if (err) {
+                    console.error("Error creating user:", err);
+                    res.status(500).send("Internal Server Error");
+                } else {
+                    console.log("User created");
+                    res.send("User created successfully");
+                }
+            });
+        }
+    });
+});
+
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
